e9af6ef9e5f93e7dc530c7d602d79996402d0883
gastrotope427 <gaurang.mishra.s64@kalvium.community>
2025-02-25 11:01:36 +0530

    done

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,16 @@
-// write the book component code here
+
+import React from "react";
+import "./bookcard.css"; // Assuming you have some styles
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="book-card">
+      <h3>{book.title}</h3>
+      <p><strong>Author:</strong> {book.author}</p>
+      <p><strong>Genre:</strong> {book.genre}</p>
+      <p><strong>Year:</strong> {book.year}</p>
+    </div>
+  );
+};
+
+export default BookCard;
